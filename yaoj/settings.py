f6023ca4e2828f6738f59c766b819e5aba4cfb43
Jiang <mail.jiang.cn@gmail.com>
2015-04-23 06:43:42 +0800

    change default setting

--- yaoj/settings.py
+++ yaoj/settings.py
@@ -72,11 +72,11 @@ WSGI_APPLICATION = 'yaoj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'yaoj',
-        'USER': 'jiang',
+        'NAME': 'postgres',
+        'USER': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
-        'PASSWORD': None,
+        'PASSWORD': 'password',
     }
 }
 
